090ee5db640260904b902feafdfe6f6983c2324b
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-07-01 14:05:41 -0600

    Initial up to part of MakeBooking.cshtml

---
+++ ModelValidation.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ModelValidation.Tests {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
---
+++ ModelValidation/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ModelValidation.Controllers {
+    public class HomeController : Controller {
+
+        public ViewResult MakeBooking() {
+            return View(new Appointment { DateTime = DateTime.Now });
+        }
+
+        [HttpPost]
+        public ViewResult MakeBooking(Appointment appt) {
+
+            //statements to store a new appointment in a 
+            //repository would go here in a real project
+
+            return View("Completed", appt);
+        }
+    }
+}
