9733a6f28a86f0d7e8ef6cba5477cfcac37d985b
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-07-01 14:24:15 -0600

    up to but not including "Displaying Property-Level Validation MEssages"

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -3,21 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ModelValidation.Models;
 
 namespace ModelValidation.Controllers {
     public class HomeController : Controller {
 
         public ViewResult MakeBooking() {
-            return View(new Appointment { DateTime = DateTime.Now });
+            return View(new Appointment { Date = DateTime.Now });
         }
 
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt) {
 
-            //statements to store a new appointment in a 
-            //repository would go here in a real project
+            if (string.IsNullOrEmpty(appt.ClientName)) {
+                ModelState.AddModelError("ClientName", "Please enter your name");
+            }
 
-            return View("Completed", appt);
-        }
+            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date) {
+                ModelState.AddModelError("Date", "Please enter a date in the future");
+            }
+
+            if(!appt.TermsAccepted) {
+                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
+            }
+
+            if (ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date")
+                && appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday) {
+                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
+            }
+
+            if (ModelState.IsValid) { 
+                //statements to store a new appointment in a
+                //repository would go here in a real project
+                return View("Completed", appt);
+            } else {
+                return View();
+            }
     }
 }
