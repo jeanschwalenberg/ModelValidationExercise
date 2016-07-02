48db2bb19edc460a97827db2c83d09b04dcfde46
JeanSchwalenberg <jeanschwalenberg@gmail.com>
2016-07-01 19:12:33 -0600

    final - although the remote validation is fucky, who cares

--- ModelValidation/Controllers/HomeController.cs
+++ ModelValidation/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using ModelValidation.Models;
 
@@ -14,30 +11,27 @@ namespace ModelValidation.Controllers {
 
         [HttpPost]
         public ViewResult MakeBooking(Appointment appt) {
-
-            if (string.IsNullOrEmpty(appt.ClientName)) {
-                ModelState.AddModelError("ClientName", "Please enter your name");
-            }
-
-            if (ModelState.IsValidField("Date") && DateTime.Now > appt.Date) {
-                ModelState.AddModelError("Date", "Please enter a date in the future");
-            }
-
-            if(!appt.TermsAccepted) {
-                ModelState.AddModelError("TermsAccepted", "You must accept the terms");
-            }
-
-            if (ModelState.IsValidField("ClientName") && ModelState.IsValidField("Date")
-                && appt.ClientName == "Joe" && appt.Date.DayOfWeek == DayOfWeek.Monday) {
-                ModelState.AddModelError("", "Joe cannot book appointments on Mondays");
-            }
-
-            if (ModelState.IsValid) { 
-                //statements to store a new appointment in a
+            if (ModelState.IsValid) {
+                //statements to store a new Appointment in a
                 //repository would go here in a real project
                 return View("Completed", appt);
             } else {
                 return View();
             }
+        }
+
+        public JsonResult ValidateDate(string Date) {
+            DateTime parsedDate;
+
+            if (!DateTime.TryParse(Date, out parsedDate)) {
+                return Json("Please enter a valid date (mm/dd/yyyy)",
+                    JsonRequestBehavior.AllowGet);
+            } else if (DateTime.Now > parsedDate) {
+                return Json("Please enter a date in the future",
+                    JsonRequestBehavior.AllowGet);
+            } else {
+                return Json(true, JsonRequestBehavior.AllowGet);
+            }
+        }
     }
 }
--- ModelValidation/Models/Appointment.cs
+++ ModelValidation/Models/Appointment.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
+using ModelValidation.Infrastructure;
+using System.Web.Mvc;
+using System.Collections.Generic;
 
 namespace ModelValidation.Models {
+    [NoJoeOnMondays]
     public class Appointment {
 
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string ClientName { get; set; }
 
         [DataType(DataType.Date)]
+        [Remote("ValidateDate", "Home")]
         public DateTime Date { get; set; }
 
         public bool TermsAccepted { get; set; }
